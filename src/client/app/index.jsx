bf01f51ed1993c55012221d6048832455a430644
pinair <riccardo.sibani@gmail.com>
2017-09-09 15:45:32 +0200

    Timer implemented

--- src/client/app/index.jsx
+++ src/client/app/index.jsx
@@ -2,11 +2,29 @@ import React from 'react';
 import { render } from 'react-dom';
 
 class App extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = { amIElected: false };
+    }
+    
+    election() {
+        this.setState({
+            amIElected: parseInt(Math.random() * 10) == 1,
+        });
+        console.log("You are elected: ", this.state.amIElected? "true" : "false");
+        console.log(this.state);
+    }
+
+    componentDidMount() {
+        this.interval = setInterval(() => this.election(), 5000);
+    }
+
     render() {
         return (
             <div>
-                <p> Hello React!@Q@@fmdi</p>
-                <p> Piotr Gay</p>
+                <p> Hello React!</p> 
+                <h1>AM I elected: {this.state.amIElected}</h1>
             </div>
         );
     }
