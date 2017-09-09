e2c84860f07647a05a71923839e08ca29212cda0
pinair <riccardo.sibani@gmail.com>
2017-09-09 15:58:30 +0200

    Smiling listener

--- Client/src/client/app/index.jsx
+++ Client/src/client/app/index.jsx
@@ -6,8 +6,36 @@ class App extends React.Component {
     constructor(props) {
         super(props);
         this.state = { amIElected: false };
+        this.listen();
     }
     
+    listen() {
+        const socket = new WebSocket(`ws://stagecast.se/api/events/comments/ws?x-user-listener=1`)
+
+        socket.onmessage = function (event) {
+
+            console.log(event.data);
+
+            let json = JSON.parse(event.data)
+            console.log("JSON", json);
+            if (json.msg == "end") {
+                console.log("END");
+            }
+
+            if (json.msg == "begin") {
+                console.log(json.msg);
+            }
+        }
+
+        socket.onerror = function () {
+            console.log("error")
+        }
+
+        socket.onopen = function () {
+            console.log("connected to socket")
+        }
+    }
+
     election() {
         this.setState({
             amIElected: parseInt(Math.random() * 10) == 1,
@@ -23,7 +51,7 @@ class App extends React.Component {
     render() {
         return (
             <div>
-                <p> Hello React!</p> 
+                <p> Hello React!</p>
                 <h1>AM I elected: {this.state.amIElected}</h1>
             </div>
         );
